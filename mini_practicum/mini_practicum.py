3a96e7ac5d575f7e75168c7ad3d6fd73282580f7
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-29 10:19:23 -0500

    Mini practicum (Part 4)

--- mini_practicum/mini_practicum.py
+++ mini_practicum/mini_practicum.py
@@ -1,3 +1,5 @@
+import math
+
 class Circle:
     __slots__ = ['__center', '__radius']
     def __init__(self, center, radius):
@@ -14,6 +16,25 @@ class Circle:
                 return True
         return False
     
+    def get_center(self):
+        return self.__center
+    
+    def get_radius(self):
+        return self.__radius
+    
+    def intersect(self, circle):
+        sum_radii = self.__radius + circle.get_radius()
+        x1 = self.__center[0]
+        x2 = circle.get_center()[0]
+        y1 = self.__center[1]
+        y2 = circle.get_center()[1]
+        distance_centers = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        
+        if sum_radii > distance_centers:
+            return True
+        
+        return False
+    
 
 def main():
     circle1 = Circle((0, 0), 2)
@@ -22,9 +43,9 @@ def main():
     a_list = [circle1, circle2, circle3]
     a_list.sort()
     print(a_list)
-    # print("circle1 and circle2 intersect:", circle1.intersect(circle2)) # True
-    # print("circle1 and circle3 intersect:", circle1.intersect(circle3)) # False
-    # print("circle2 and circle3 intersect:", circle2.intersect(circle3)) # True
+    print("circle1 and circle2 intersect:", circle1.intersect(circle2)) # True
+    print("circle1 and circle3 intersect:", circle1.intersect(circle3)) # False
+    print("circle2 and circle3 intersect:", circle2.intersect(circle3)) # True
 
 main()           
     
