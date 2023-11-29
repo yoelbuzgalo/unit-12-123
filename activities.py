cd080e36410b1f41344e2a6893bf58d111238b0b
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-29 11:16:00 -0500

    Activity 12.5

--- activities.py
+++ activities.py
@@ -7,9 +7,17 @@ def length(node, count=0):
         count += 1
         return length(node.get_next(), count)
 
+def total(node, total_counted=0):
+    if node == None:
+        return total_counted
+    else:
+        total_counted += node.get_value()
+        return total(node.get_next(), total_counted)
+
 def main():
     seq = Node(3, Node(2, Node(1)))
     print(length(seq))
+    print(total(seq))
 
 if __name__ == "__main__":
     main()
