ee51baf0d428bb40e7c1a2452bbe80c07cba9b3e
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-29 11:18:55 -0500

    Activity 12.6

--- activities.py
+++ activities.py
@@ -1,18 +1,28 @@
 from node import Node
 
 def length(node, count=0):
-    if node == None:
-        return count
-    else:
+    # if node == None:
+    #     return count
+    # else:
+    #     count += 1
+    #     return length(node.get_next(), count)
+    count = 0
+    while node is not None:
         count += 1
-        return length(node.get_next(), count)
+        node = node.get_next()
+    return count
 
 def total(node, total_counted=0):
-    if node == None:
-        return total_counted
-    else:
-        total_counted += node.get_value()
-        return total(node.get_next(), total_counted)
+    # if node == None:
+    #     return total_counted
+    # else:
+    #     total_counted += node.get_value()
+    #     return total(node.get_next(), total_counted)
+    total = 0
+    while node is not None:
+        total += node.get_value()
+        node = node.get_next()
+    return total
 
 def main():
     seq = Node(3, Node(2, Node(1)))
