8ca682e5db904acda77291c4feb74fba0b5e9e67
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-29 11:49:49 -0500

    Activity 12.9

--- node_stack.py
+++ node_stack.py
@@ -1,3 +1,5 @@
+from node import Node
+
 class Stack:
     __slots__ = ['__top']
     def __init__(self):
@@ -8,3 +10,13 @@ class Stack:
             return True
         return False
     
+    def __stringify__(self, node, string=""):
+        # Return all the values in the tack as a comma seperated string
+        if node is None:
+            return string
+        else:
+            string += str(node.get_value()) + ", "
+            return Stack.__stringify__(node.get_next(), string)
+        
+    def __str__(self):
+        return "[" + Stack.__stringify__(self.__top) + "]"
