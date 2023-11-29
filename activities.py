c7d54d5708e3183099f4b2a6582daea4ff3bb957
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-29 11:07:03 -0500

    Activity 12.4

--- activities.py
+++ activities.py
@@ -1,8 +1,15 @@
 from node import Node
 
+def length(node, count=0):
+    if node == None:
+        return count
+    else:
+        count += 1
+        return length(node.get_next(), count)
+
 def main():
     seq = Node(3, Node(2, Node(1)))
-    print(seq)
+    print(length(seq))
 
 if __name__ == "__main__":
     main()
