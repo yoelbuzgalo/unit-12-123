8a5b535913fdefe75bee4cc63fe64a247d14cb88
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-01 11:22:09 -0500

    Activity 12.16

--- activities.py
+++ activities.py
@@ -24,13 +24,25 @@ def total(node, total_counted=0):
         node = node.get_next()
     return total
 
-# Activity 12.7, Visualizing Stack Operations:
-# [5]
-# [5,8]
-# [5]
-# [5,3]
-# [5]
-# [5,1]
+
+
+"""
+Activity 12.7, Visualizing Stack Operations:
+[5]
+[5,8]
+[5]
+[5,3]
+[5]
+[5,1]
+
+Activity 12.16, Visualizing Queue:
+[5]
+[5,8]
+[8]
+[8,3]
+[3]
+[3,1]
+"""
 
 def main():
     seq = Node(3, Node(2, Node(1)))
