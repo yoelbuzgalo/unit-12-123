85f166868eaa5564e92ba7d0b047b7d1838f7c18
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-02 20:11:58 -0500

    Assignment 12.1 (More testing, asserting string is correct)

--- stacks_test.py
+++ stacks_test.py
@@ -50,4 +50,5 @@ def test_list_stack_content():
     top_character_in_stack = top_line_in_main.peek()
 
     # Analysis
+    assert str(top_line_in_main) == "[T, h, e,  , t, i, m, e,  , h, a, s,  , c, o, m, e, ,, ',  , t, h, e,  , W, a, l, r, u, s,  , s, a, i, d, ,]"
     assert top_character_in_stack == "T"
