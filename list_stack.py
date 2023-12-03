501042ead5eaea815e1f0eb16e8373d8e290d42c
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-02 21:36:25 -0500

    Assignment 12.1 Fixed list_stack completely to list based

--- list_stack.py
+++ list_stack.py
@@ -1,46 +1,31 @@
-from node import Node
-
 class Stack:
-    __slots__ = ['__top', '__size']
+    __slots__ = ['__list', '__size']
     def __init__(self):
-        self.__top = None
+        self.__list = []
         self.__size = 0
 
     def is_empty(self):
-        if self.__top == None:
+        if self._list == []:
             return True
         return False
-    
-    def __stringify(node, string=""):
-        # Return all the values in the stack as a comma seperated string
-        if node == None:
-            return string
-        else:
-            string += str(node.get_value()) + ", "
-            return Stack.__stringify(node.get_next(), string)
         
     def __str__(self):
-        return "[" + Stack.__stringify(self.__top)[:-2] + "]"
+        return str(self.__list)
     
     def __len__(self):
         return self.__size
     
     def peek(self):
-        if self.__top == None:
+        if self.__list[-1] == None:
             raise IndexError("Can't peek an empty queue")
-        return self.__top.get_value()
+        return self.__list[-1]
     
     def pop(self):
-        if self.__top == None:
+        if self.__list[0] == None:
             raise IndexError("Cant pop an empty queue")
-        value = self.__top.get_value()
-        self.__top = self.__top.get_next()
         self.__size -= 1
-        
-        return value
+        return self.__list.pop()
 
     def push(self, value):
-        new_node = Node(value)
-        new_node.set_next(self.__top)
-        self.__top = new_node
+        self.__list.append(value)
         self.__size += 1
