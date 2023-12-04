988cf872880c6cca1e17937a2f438f53a61b78e9
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-12-04 11:12:45 -0500

    Activity 12.27

--- droids.py
+++ droids.py
@@ -1,6 +1,43 @@
 PROTO = "Protocol"
 ASTRO = "Astro"
 
-PROTO_PARTS = {'p_head', 'p_chasis', 'p_left_arm', 'p_right_arm', ' p_left_leg', 'p_right_leg'}
+PARTS = {
+    PROTO: {'p_head', 'p_chasis', 'p_left_arm', 'p_right_arm', ' p_left_leg', 'p_right_leg'},
+    ASTRO: {'a_dome', 'a_body', 'a_left_leg', 'a_center_leg', 'a_right_leg'}
+    }
 
-ASTRO_PARTS = {'a_dome', 'a_body', 'a_left_leg', 'a_center_leg', 'a_right_leg'}
+class Droid:
+    __slots__ = ['__serial_number','__type', '__parts']
+    def __init__(self, serial_number, type):
+        self.__serial_number = serial_number
+        self.__type = type
+        self.__parts = set()
+
+
+    def needs_part(self, a_part):
+        if a_part not in self.__parts:
+            return True
+        
+    def install(self, a_part):
+        if self.needs_part(a_part):
+            self.__parts.add(a_part)
+        else:
+            raise ValueError("Part already exists")
+        
+    def is_complete(self):
+        for part in PROTO[self.__type]:
+            if self.needs_part(part):
+                return False
+        return True
+    
+    def __repr__(self):
+        return "Serial Number:" + str(self.__serial_number) \
+        + "\n Parts:" + str(self.__parts)
+    
+def main():
+    droid = Droid(1, PROTO)
+    droid.install('p_head')
+    print(droid)
+
+if __name__ == "__main__":
+    main()
