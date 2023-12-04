5b6dc59167aafd0235efc6d857b1c5e8b9596ad0
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-12-04 10:54:44 -0500

    Activity 12.25

---
+++ droids.py
@@ -0,0 +1,6 @@
+PROTO = "Protocol"
+ASTRO = "Astro"
+
+PROTO_PARTS = {'p_head', 'p_chasis', 'p_left_arm', 'p_right_arm', ' p_left_leg', 'p_right_leg'}
+
+ASTRO_PARTS = {'a_dome', 'a_body', 'a_left_leg', 'a_center_leg', 'a_right_leg'}
