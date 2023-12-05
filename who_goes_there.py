34e706d5c1ab964c69d1e135e463c4662ea6c023
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-05 01:17:55 -0500

    Assignment 12.2 (Part 1a)

---
+++ who_goes_there.py
@@ -0,0 +1,27 @@
+class Task:
+    """
+    This is a task class that stores in a name and location of the task itself
+    """
+    __slots__ = ['__name', '__location']
+
+    def __init__(self, name, location):
+        self.__name = name
+        self.__location = location
+
+    def get_name(self):
+        return self.__name
+    
+    def get_location(self):
+        return self.__location
+    
+    def set_name(self, name):
+        self.__name = name
+    
+    def set_location(self, location):
+        self.__location = location
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
