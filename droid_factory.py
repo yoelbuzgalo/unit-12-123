064a1e064ca07c17e55dd8835d7a360309b8757b
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-06 11:17:00 -0500

    Activity 12.32

--- droid_factory.py
+++ droid_factory.py
@@ -17,15 +17,33 @@ def install_part(droid, my_belt, their_belt):
 
     return droid.is_complete()
 
+def assemble_droids(worker_belt, coworker_belt):
+
+    p_count = 0
+    a_count =0
+
+    p_droid = droids.Droid("P"+str(p_count), droids.PROTO)
+    a_droid = droids.Droid("A"+str(a_count), droids.ASTRO)
+    done = False
+    while not worker_belt.is_empty() or not coworker_belt.is_empty():
+        if install_part(p_droid, worker_belt, coworker_belt):
+            print(p_droid)
+            p_count += 1
+            p_droid = droids.Droid("P"+str(p_count), droids.PROTO)
+        if install_part(a_droid, worker_belt, coworker_belt):
+            print(a_droid)
+            a_count += 1
+            a_droid = droids.Droid("A"+str(a_count), droids.ASTRO)
+
 def main():
     p_belt = Queue()
     a_belt = Queue()
     unload_shipment("parts_0001_0001.txt", p_belt)
-    print(p_belt)
 
     astro = droids.Droid(1, droids.ASTRO)
     install_part(astro, p_belt, a_belt)
-    print(p_belt)
+
+    assemble_droids(p_belt, a_belt)
 
 if __name__ == "__main__":
     main()
